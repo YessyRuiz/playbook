38f0615abcd9f080aa219534200574eb05e2f24e
Yessica Ruiz <99302667+YessyRuiz@users.noreply.github.com>
2022-05-06 07:22:51 -0700

    Ejemplo con pokemon

---
+++ weekly_mission_1/example9/main.js
@@ -0,0 +1,17 @@
+const pokemon = require('./pokemon')
+const pikachu = new pokemon ("pikachu")
+const bulbasor = new pokemon ("bulbasor")
+const squirtle = new pokemon ("squirtle")
+const charmander = new pokemon ("charmander")
+
+pikachu.sayHello()
+pikachu.sayMessage("Heey!")
+
+bulbasor.sayHello()
+bulbasor.sayMessage("Heey!")
+
+squirtle.sayHello()
+squirtle.sayMessage("Heey!")
+
+charmander.sayHello()
+charmander.sayMessage("Heey!")
